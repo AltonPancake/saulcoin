5c1f7d4d95824b57a5978e3df1aeb2e708b38aea
AltonPancake <noah.alton8@gmail.com>
2023-04-13 14:29:39 -0500

    working saves and better UI

--- js/index.js
+++ js/index.js
@@ -1,221 +1,138 @@
-var click = 0;
-var amnt = 1;
-var score = 0;
-var automine = 0;
-var automine2 = 0;
-var fastmine = 0;
-var bigmine = 0;
-var automineUpgrade = 500;
-var automineUpgrade2 = 2500;
-var fastmineUpgrade = 750;
-var bigmineUpgrade = 1000;
-var upgradeCost = 25;
-var clickUse = 7;
-var autoCount1 = 0;
-var autoCount2 = 0;
-var autoCount3 = 0;
+ 
+var ss = { 
+ click: 0,
+ amnt: 1,
+ score: 0,
+ upgradeCost: 25,
+ junkAutominerCost: 50,
+ badAutominerCost: 250,
+ okayAutominerCost: 750,
+ goodAutominerCost: 1500,
+ sps: 0, 
+ saulTouch: 100000,
+ time: 2147483647
+}
 
-var howManySaul = window.setInterval(worthOfSaul, 2500);
-var saulflation = 10;
+if(localStorage.getItem("data") != null){
+  ss = JSON.parse(localStorage.getItem("data"));
+}
 
-var fastmining = window.setInterval(fastMine, 750);
-var automining = window.setInterval(autoMine, 1000);
-var automining2 = window.setInterval(autoMine2, 500);
-var bigmining = window.setInterval(bigMine, 4000);
+var automining = window.setInterval(addScore, ss.time);
 
-document.getElementById("upgrade2").style.display = 'none';
-document.getElementById("upgrade3").style.display = 'none';
-document.getElementById("upgrade4").style.display = 'none';
+var save = window.setInterval(saveState, 60000);
+
 document.getElementById("touchOfMidas").style.display = 'none';
 
+document.getElementById("junkAutominer").innerHTML = ("Junk Autominer (-" + ss.junkAutominerCost + ") - +1 saulcoin per second");
+document.getElementById("badAutominer").innerHTML = ("Bad Autominer (-" + ss.badAutominerCost + ") - +5 saulcoins per second");
+document.getElementById("okayAutominer").innerHTML = ("Okay Autominer (-" + ss.okayAutominerCost + ") - +25 saulcoins per second");
+document.getElementById("goodAutominer").innerHTML = ("Good Autominer (-" + ss.goodAutominerCost + ") - +50 saulcoins per second");
+document.getElementById("scps").innnerHTML = ("saulcoins per second: " + ss.sps);
+
+document.getElementById("junkAutominer").onclick = function(){
+  if(ss.score >= ss.junkAutominerCost){
+  ss.sps = ss.sps + 1;
+  ss.score = ss.score - ss.junkAutominerCost;
+  ss.click = ss.score;
+  ss.junkAutominerCost = Math.round(ss.junkAutominerCost * 1.25);
+  ss.time = 1000/ss.sps;
+  document.getElementById("score").innerHTML = ("saulcoins: " + ss.click);
+  document.getElementById("junkAutominer").innerHTML = ("Junk Autominer (-" + ss.junkAutominerCost + " saulcoins) - +1 saulcoin per second");
+  document.getElementById("scps").innerHTML = ("saulcoins per second: " + ss.sps);
+  window.clearInterval(automining);
+  automining = window.setInterval(addScore, ss.time);
+  } else {
+    alert("ur literally 2 poor lmao");
+  }
+}
+
+document.getElementById("badAutominer").onclick = function(){
+  if(ss.score >= ss.badAutominerCost){
+  ss.sps = ss.sps + 5;
+  ss.score = ss.score - ss.badAutominerCost;
+  ss.click = ss.score;
+  ss.badAutominerCost = Math.round(ss.badAutominerCost * 1.5);
+  ss.time = 1000/ss.sps;
+  document.getElementById("score").innerHTML = ("saulcoins: " + ss.click);
+  document.getElementById("badAutominer").innerHTML = ("Bad Autominer (-" + ss.badAutominerCost + " saulcoins) - +5 saulcoins per second");
+  document.getElementById("scps").innerHTML = ("saulcoins per second: " + ss.sps);
+  window.clearInterval(automining);
+  automining = window.setInterval(addScore, ss.time);
+  } else {
+    alert("ur literally 2 poor lmao");
+  }
+}
+
+document.getElementById("okayAutominer").onclick = function(){
+  if(ss.score >= ss.okayAutominerCost){
+  ss.sps = ss.sps + 25;
+  ss.score = ss.score - ss.okayAutominerCost;
+  ss.click = ss.score;
+  ss.okayAutominerCost = Math.round(ss.okayAutominerCost * 1.75);
+  ss.time = 1000/ss.sps;
+  document.getElementById("score").innerHTML = ("saulcoins: " + ss.click);
+  document.getElementById("okayAutominer").innerHTML = ("Okay Autominer (-" + ss.okayAutominerCost + " saulcoins) - +10 saulcoins per second");
+  document.getElementById("scps").innerHTML = ("saulcoins per second: " + ss.sps);
+  window.clearInterval(automining);
+  automining = window.setInterval(addScore, ss.time);
+  } else {
+    alert("ur literally 2 poor lmao");
+  }
+}
+
+document.getElementById("goodAutominer").onclick = function(){
+  if(ss.score >= ss.goodAutominerCost){
+  ss.sps = ss.sps + 50;
+  ss.score = ss.score - ss.goodAutominerCost;
+  ss.click = ss.score;
+  ss.goodAutominerCost = Math.round(ss.goodAutominerCost * 2);
+  ss.time = 1000/ss.sps;
+  document.getElementById("score").innerHTML = ("saulcoins: " + ss.click);
+  document.getElementById("goodAutominer").innerHTML = ("Good Autominer (-" + ss.goodAutominerCost + " saulcoins) - +50 saulcoins per second");
+  document.getElementById("scps").innerHTML = ("saulcoins per second: " + ss.sps);
+  window.clearInterval(automining);
+  automining = window.setInterval(addScore, ss.time);
+  } else {
+    alert("ur literally 2 poor lmao");
+  }
+}
+
 document.getElementById("upgrade").onclick = function() {
-  if (score >= upgradeCost) {
-    score = score - upgradeCost;
-    click = score;
-    //console.log(click);
-    document.getElementById("score").innerHTML = ("saulcoins: " + click);
-    upgradeCost = Math.round(upgradeCost * 1.2);
-    document.getElementById("upgrade").innerHTML = ("upgrade +1 per click (- " + upgradeCost + " saulcoins)");
-    amnt = amnt + 1;
-    saulflation = saulflation * (amnt / 4);
-    clickUse = clickUse - 1;
-    document.getElementById("clickie").innerHTML = ("+ " + amnt + " per click")
-  } else {
-    alert("ur literally 2 poor lmao");
-  }
-  if (clickUse <= 0) {
-    document.getElementById("upgrade").style.display = 'none';
-    document.getElementById("upgrade2").style.display = 'initial';
-    document.getElementById("upgrade2").innerHTML = ("upgrade +2 per click (-" + upgradeCost + " saulcoins");
-  }
-}
-
-document.getElementById("upgrade2").onclick = function() {
-  if (score >= upgradeCost) {
-    score = score - upgradeCost;
-    click = score;
-    //console.log(click);
-    document.getElementById("score").innerHTML = ("saulcoins: " + click);
-    upgradeCost = Math.round(upgradeCost * 1.5);
-    document.getElementById("upgrade2").innerHTML = ("upgrade +2 per click (-" + upgradeCost + " saulcoins)");
-    amnt = amnt + 2;
-    saulflation = saulflation * (amnt / 4);
-    document.getElementById("clickie").innerHTML = ("+ " + amnt + " per click");
-  } else {
-    alert("ur literally 2 poor lmao");
-  }
-  if (amnt >= 30) {
-    document.getElementById("upgrade2").style.display = 'none';
-    document.getElementById("upgrade3").style.display = 'initial';
-    document.getElementById("upgrade3").innerHTML = ("upgrade +5 per click (-" + upgradeCost + " saulcoins)");
-  }
-}
-
-document.getElementById("upgrade3").onclick = function() {
-  if (score >= upgradeCost) {
-    score = score - upgradeCost;
-    click = score;
-    //console.log(click);
-    document.getElementById("score").innerHTML = ("saulcoins: " + click);
-    upgradeCost = Math.round(upgradeCost * 1.5);
-    document.getElementById("upgrade3").innerHTML = ("upgrade +5 per click (-" + upgradeCost + " saulcoins)");
-    amnt = amnt + 5;
-    saulflation = saulflation * (amnt / 4);
-    document.getElementById("clickie").innerHTML = ("+ " + amnt + " per click");
-  } else {
-    alert("ur literally 2 poor lmao");
-  }
-  if (amnt >= 50) {
-    document.getElementById("upgrade3").style.display = 'none';
-    document.getElementById("upgrade4").style.display = 'initial';
-    document.getElementById("upgrade4").innerHTML = ("upgrade +10 per click (-" + upgradeCost + " saulcoins)");
-    document.getElementById("touchOfMidas").style.display = 'initial';
+  if (ss.amnt <= 10){
+    upgrade1();
+    } else if (ss.amnt <= 30){
+      upgrade2();
+    } else if (ss.amnt <= 50){
+      upgrade3();
   }
 }
 
 document.getElementById("touchOfMidas").onclick = function() {
-  if (score >= 100000) {
-    score = score - 100000;
-    click = score;
+  if (ss.score >= ss.saulTouch) {
+    ss.score = ss.score - ss.saulTouch;
+    ss.click = ss.score;
+    ss.amnt = ss.amnt * 2
+    ss.saulTouch = ss.saulTouch * 5
     document.getElementById("score").innerHTML = ("saulcoins")
-    document.getElementById("clickie").innerHTML = ("+ " + amnt + " per click");
+    document.getElementById("clickie").innerHTML = ("+ " + ss.amnt + " per click");
   }
 }
 
-document.getElementById("upgrade4").onclick = function() {
-  if (score >= upgradeCost) {
-    score = score - upgradeCost;
-    click = score;
-    //console.log(click);
-    document.getElementById("score").innerHTML = ("saulcoins: " + click);
-    upgradeCost = Math.round(upgradeCost * 1.5);
-    document.getElementById("upgrade4").innerHTML = ("upgrade +10 per click (-" + upgradeCost + " saulcoins)");
-    amnt = amnt + 10;
-    saulflation = saulflation * (amnt / 10);
-    document.getElementById("clickie").innerHTML = ("+ " + amnt + " per click");
-  } else {
-    alert("ur literally 2 poor lmao");
-  }
-}
-
-document.getElementById("autominers").onclick = function() {
-  if (score >= automineUpgrade) {
-    score = score - automineUpgrade;
-    click = score;
-    //console.log(click);
-    document.getElementById("score").innerHTML = ("saulcoins: " + click);
-    automineUpgrade = Math.round(automineUpgrade * 1.7);
-    automine = automine + 1;
-    document.getElementById("autominers").innerHTML = ("autominer (- " + automineUpgrade + " saulcoins) - - - you have " + automine + " autominer(s)");
-  } else {
-    alert("ur literally 2 poor lmao");
-  }
-}
-
-document.getElementById("autominers2").onclick = function() {
-  if (score >= automineUpgrade2) {
-    score = score - automineUpgrade2;
-    click = score;
-    //console.log(click);
-    document.getElementById("score").innerHTML = ("saulcoins: " + click);
-    automineUpgrade2 = Math.round(automineUpgrade2 * 2);
-    automine2 = automine2 + 5;
-    autoCount2 = autoCount2 + 1;
-    document.getElementById("autominers2").innerHTML = ("better autoiner (5x speed & earnings) (-" + automineUpgrade2 + " saulcoins) - - - you have " + autoCount2 + " better autominer(s)");
-  } else {
-    alert("ur literally 2 poor lmao");
-  }
-}
-
-document.getElementById("fasterMiner").onclick = function() {
-  if (score >= fastmineUpgrade) {
-    score = score - fastmineUpgrade;
-    click = score;
-    //console.log(click);
-    document.getElementById("score").innerHTML = ("saulcoins: " + click);
-    fastmineUpgrade = Math.round(fastmineUpgrade * 1.5);
-    fastmine = fastmine + 1;
-    autoCount1 = autoCount1 + 1;
-    document.getElementById("fasterMiner").innerHTML = ("faster autominer (1.5x speed) (-" + fastmineUpgrade + " saulcoins) - - - you have " + autoCount1 + " faster autominer(s)");
-  } else {
-    alert("ur literally 2 poor lmao");
-  }
-}
-
-document.getElementById("bigMiner").onclick = function() {
-  if (score >= bigmineUpgrade) {
-    score = score - bigmineUpgrade;
-    click = score;
-    //console.log(click);
-    document.getElementById("score").innerHTML = ("saulcoins: " + click);
-    bigmineUpgrade = Math.round(bigmineUpgrade * 1.75);
-    bigmine = bigmine + 10;
-    autoCount3 = autoCount3 + 1;
-    document.getElementById("bigMiner").innerHTML = ("bigger autominer (10x earnings, 0.25x speed) (-" + bigmineUpgrade + " saulcoins) - - - you have " + autoCount3 + " bigger autominer(s)");
-  } else {
-        alert("ur literally 2 poor lmao");
-        }
+function saveState(){
+  localStorage.setItem("data", JSON.stringify(ss));
 }
 
 document.getElementById("saul").onclick = function() {
-  score = score + amnt;
-  click = score;
+  ss.score = ss.score + ss.amnt;
+  ss.click = ss.score;
   //console.log(click);
-  document.getElementById("score").innerHTML = ("saulcoins: " + click);
-
+  document.getElementById("score").innerHTML = ("saulcoins: " + ss.click);
 }
 
-async function autoMine() {
-  score = score + automine;
-  click = score;
-  document.getElementById("score").innerHTML = ("saulcoins: " + click);
-}
-
-async function autoMine2() {
-  score = score + automine2
-  click = score;
-  document.getElementById("score").innerHTML = ("saulcoins: " + click);
-}
-
-async function fastMine() {
-  score = score + fastmine;
-  click = score;
-  document.getElementById("score").innerHTML = ("saulcoins: " + click);
-}
-
-async function bigMine() {
-  score = score + bigmine;
-  click = score;
-  document.getElementById("score").innerHTML = ("saulcoins: " + click);
-}
-
-async function worthOfSaul() {
-  var saulWorth = getRandomWorth(saulflation);
-  document.getElementById("saulcoinWorth").innerHTML = ("1 saulcoin is worth: $" + saulWorth + " USD");
-}
-
-function getRandomWorth(max) {
-  return Math.floor(Math.random() * max);
+function addScore(){
+  ss.score = ss.score + 1;
+  ss.click = ss.score;
+  document.getElementById("score").innerHTML = ("saulcoins: " + ss.click);
 }
 
 var pattern = ['n', 'e', 'r', 'd'];
@@ -232,23 +149,56 @@ var keyHandler = function(event) {
 
   if (pattern.length === current) {
     current = 0;
-    score = score + 10000000000000;
-    click = score;
-    document.getElementById('score').innerHTML = ('saulcoins: ' + click);
+    ss.score = ss.score + 10000;
+    ss.click = ss.score;
+    document.getElementById('score').innerHTML = ('saulcoins: ' + ss.click);
             gamer.play();
 
   }
 }
 document.addEventListener('keydown', keyHandler, false);
 
-document.getElementById('settingsMenu').style.display = 'none';
-
-document.getElementById('openMenu').onclick = function(){
-    document.getElementById('openMenu').style.display = 'none';
-    document.getElementById('settingsMenu').style.display = 'initial';
+function upgrade1(){
+  if (ss.score >= ss.upgradeCost) {
+    ss.score = ss.score - ss.upgradeCost;
+    ss.click = ss.score;
+    //console.log(click);
+    document.getElementById("score").innerHTML = ("saulcoins: " + ss.click);
+    ss.upgradeCost = Math.round(ss.upgradeCost * 1.2);
+    document.getElementById("upgrade").innerHTML = ("upgrade +1 per click (- " + ss.upgradeCost + " saulcoins)");
+    ss.amnt = ss.amnt + 1;
+    document.getElementById("clickie").innerHTML = ("+ " + ss.amnt + " per click")
+  } else {
+      alert("ur literally 2 poor lmao");
+    }
 }
 
-document.getElementById('closeMenu').onclick = function(){
-    document.getElementById('openMenu').style.display = 'initial';
-    document.getElementById('settingsMenu').style.display = 'none';
+function upgrade2(){
+  if (ss.score >= ss.upgradeCost) {
+    ss.score = ss.score - ss.upgradeCost;
+    ss.click = ss.score;
+    //console.log(click);
+    document.getElementById("score").innerHTML = ("saulcoins: " + ss.click);
+    ss.upgradeCost = Math.round(ss.upgradeCost * 1.5);
+    document.getElementById("upgrade").innerHTML = ("upgrade +2 per click (-" + ss.upgradeCost + " saulcoins)");
+    ss.amnt = ss.amnt + 2;
+    document.getElementById("clickie").innerHTML = ("+ " + ss.amnt + " per click");
+  } else {
+    alert("ur literally 2 poor lmao");
+  }
+}
+
+function upgrade3(){
+  if (ss.score >= ss.upgradeCost) {
+    ss.score = ss.score - ss.upgradeCost;
+    ss.click = ss.score;
+    //console.log(click);
+    document.getElementById("score").innerHTML = ("saulcoins: " + ss.click);
+    ss.upgradeCost = Math.round(ss.upgradeCost * 1.5);
+    document.getElementById("upgrade").innerHTML = ("upgrade +5 per click (-" + ss.upgradeCost + " saulcoins)");
+    ss.amnt = ss.amnt + 5;
+    document.getElementById("clickie").innerHTML = ("+ " + ss.amnt + " per click");
+  } else {
+    alert("ur literally 2 poor lmao");
+  }
 }
